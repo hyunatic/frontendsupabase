b002ac312fc61fd09b76cd423a52a92cda54a7de
hyunatic <habbo_armageddon@hotmail.com>
2022-07-06 02:34:00 +0000

    Deploying to gh-pages from @ hyunatic/frontendsupabase@23f764ed9027d4ea1556430b88dc3b82eaf82135 🚀

---
+++ _next/static/FpCte8qlDM5royahayKJi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/111-1f51da6ae8925af1.js","static/chunks/pages/index-543a424119d88268.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
